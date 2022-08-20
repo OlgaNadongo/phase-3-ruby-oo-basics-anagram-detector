a99c34f80b7dfd6b9c2cb5411fdc3a7e01b79355
Olga Nadongo <winnienadongo@gmail.com>
2022-08-20 06:49:37 +0300

    complete lab

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+
+
+    def initialize(word)
+        @word=word
+    end
+
+    def match(probable_anagrams)
+        anagrams = []
+        probable_anagrams.each do |probable_anagram|
+            if  probable_anagram.chars.sort == @word.chars.sort
+         
+                anagrams << probable_anagram
+            end
+        end
+        anagrams
+    end
+end
